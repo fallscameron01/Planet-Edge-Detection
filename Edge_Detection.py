31b1cfd495efa2dfe437134c7110fe4afa723ef7
Cameron Falls <camfalls0201@gmail.com>
2020-03-27 17:33:30 -0500

    Various Updates

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -1,43 +1,37 @@
 import numpy as np
-import matplotlib.image as mpimg
+
 import matplotlib.pyplot as plt
+
 from scipy.ndimage import generic_filter, convolve
+
 import cv2
 
 # Initial Image
-image = mpimg.imread('images/Mars.jpg')
-plt.imshow(image)
-plt.show()
+image = cv2.imread('images/Mars.jpg')
+print(image)
 
 # K Means Clustering
 vec = image.reshape((-1, 3))
 vec = np.float32(vec)
 
 termination_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-
 K = 2
 _, label, center = cv2.kmeans(vec, K, bestLabels=None, criteria=termination_criteria, attempts=10, flags=0)
 
 meansImg = np.uint8(center)[label.flatten()]
 meansImg = meansImg.reshape((image.shape))
 
-plt.imshow(meansImg)
-plt.show()
-
 # Edge Detection
-edge_kernel = [
+edge_kernel = np.array((
     [-1, -1, -1], 
     [-1, 8, -1], 
-    [-1, -1, -1]]
-
-edge_kernel = np.array(edge_kernel)
+    [-1, -1, -1]))
 
 grayscaleImg = np.mean(meansImg, axis=2)
-plt.imshow(grayscaleImg, cmap='gray')
-plt.show()
 
 edges = convolve(grayscaleImg, edge_kernel)
 binarized_edges = np.where(edges > .25, 1, 0)
 
-plt.imshow(binarized_edges, cmap='gray')
-plt.show()
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

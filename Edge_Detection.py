35c39b383394a95789bcbb67b04065888f24a120
Cameron Falls <camfalls0201@gmail.com>
2020-04-02 20:05:51 -0500

    Optimizations and Implement as Function

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -1,36 +1,39 @@
 import numpy as np
-
-import matplotlib.pyplot as plt
-
 from scipy.ndimage import generic_filter, convolve
-
 import cv2
 
-# Initial Image
-image = cv2.imread('images/Mars.jpg')
 
-# K Means Clustering
-vec = image.reshape((-1, 3))
-vec = np.float32(vec)
+def PlanetDetection(imagepath):
+    # Initial Image
+    image = cv2.imread('images/Mars.jpg')
 
-termination_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 2
-_, label, center = cv2.kmeans(vec, K, bestLabels=None, criteria=termination_criteria, attempts=10, flags=0)
+    # K Means Clustering
+    vec = image.reshape((-1, 3))
+    vec = np.float32(vec)
 
-meansImg = np.uint8(center)[label.flatten()]
-meansImg = meansImg.reshape((image.shape))
+    termination_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 2
+    _, label, center = cv2.kmeans(vec, K, bestLabels=None, criteria=termination_criteria, attempts=10, flags=0)
 
-# Edge Detection
-edge_kernel = np.array((
-    [-1, -1, -1], 
-    [-1, 8, -1], 
-    [-1, -1, -1]))
+    meansImg = np.uint8(center)[label.flatten()]
+    meansImg = meansImg.reshape((image.shape))
 
-grayscaleImg = np.mean(meansImg, axis=2)
+    # Edge Detection
+    edge_kernel = np.array((
+        [-1, -1, -1], 
+        [-1, 8, -1], 
+        [-1, -1, -1]))
 
-edges = convolve(grayscaleImg, edge_kernel)
-binarized_edges = np.where(edges > .25, 1, 0)
+    grayscaleImg = np.mean(meansImg, axis=2)
 
-cv2.imshow("Edges", edges)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    edges = convolve(grayscaleImg, edge_kernel)
+    binarized_edges = np.where(edges > .25, 1, 0)
+
+    cv2.imshow("edges", edges)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    imgpath = 'images/Mars.jpg'
+    PlanetDetection(imgpath)
